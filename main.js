308ddd4b254cb9b8e02a8492c7e6f9f5919a7f30
Isabel Holland <isabel@Isabels-MacBook-Air.local>
2020-09-30 14:04:41 +0100

    minute hand added

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
 const hand = document.querySelector(".hand");
+const minhand = document.querySelector(".minhand");
 //hand.style.transform.transform-origin =10%;
 //hand.style.transform = `rotate(90deg)`; // example of how to rotate the hand
 let second =  0; 
+let minute = 0;
 
 function handRotate(){
 
 hand.style.transform = `rotate(+${second}deg)`;
-second = second +6;
-console.log("hello");
+    second = second +6;
+    console.log("hello");
 }
 
+
+/* MINUTE HAND
 let secondHand = setInterval(handRotate, 1000); 
 
+function minhandRotate(){
 
+    hand.style.transform = `rotate(+${minute}deg)`;
+        minute = minute +6;
+        console.log("hello");
+    }
+    
+let minuteHand = setInterval(minhandRotate, 10000); 
 
+*/
 
-//"rotate(" + sdegree + "deg)";
-//handRotate();
 
 
 // define the seconds hand element in HTML to JS
