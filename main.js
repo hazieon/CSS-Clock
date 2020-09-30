14b68ec9e070f34eaea89039e05c2fde63cf7c7a
Isabel Holland <isabel@Isabels-MacBook-Air.local>
2020-09-30 12:11:09 +0100

    second hand rotation added

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 const hand = document.querySelector(".hand");
 
-hand.style.transform = `rotate(90deg)`; // example of how to rotate the hand
+//hand.style.transform = `rotate(90deg)`; // example of how to rotate the hand
+
+function handRotate(){
+    hand.style.transform = `rotate(6deg)`;   
+}
+
+handRotate();
+
+
+// define the seconds hand element in HTML to JS
+// create a function that rotates the second hand by 6 degrees
+// use setInterval to run every second (1000 ms)
+// 
+//
